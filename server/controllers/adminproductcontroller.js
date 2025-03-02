a06cedca8012bc05e8902682c66560b8cda44169
rhv1501 <rhv4748@gmail.com>
2025-03-02 20:01:27 +0530

    further work on admin product controller

--- server/controllers/adminproductcontroller.js
+++ server/controllers/adminproductcontroller.js
@@ -1,29 +1,52 @@
-import { request } from "express";
 import Order from "../models/order.model.js";
 import ProductModel from "../models/product.model.js";
-export const orders = (req, res) => {
+export const orders = async (req, res) => {
   try {
     const user_id = req.user.user_id;
     if (!user_id) {
       res.status(200).json({ message: "user un-authorized" });
+      return;
     }
-    const orders = Order.find();
+    const orders = await Order.find();
     res.status(200).json({ orders });
   } catch (e) {
     res.status(500).json({ error: e, message: "Internal server error" });
   }
 };
 
-export const products = (req, res) => {
+export const products = async (req, res) => {
   try {
     const user_id = req.user.user_id;
     if (!user_id) {
       res.status(200).json({ message: "user un-authorized" });
+      return;
     }
-    const products = ProductModel.find();
+    const products = await ProductModel.find();
     res.status(200).json({ products });
   } catch (e) {
     res.status(500).json({ error: e, message: "Internal server error" });
   }
 };
-export const addproducts = (req, res) => {};
+export const addproducts = async (req, res) => {
+  const { name, description, price, category, stock } = req.body;
+  try {
+    const user_id = req.user.user_id;
+    if (!user_id) {
+      res.status(200).json({ message: "user un-authorized" });
+      return;
+    }
+    const product = new ProductModel({
+      name,
+      description,
+      price,
+      category,
+      stock,
+    });
+    product.save();
+    res.status(200).json({ message: "product added successfully", product });
+  } catch (e) {
+    res.status(500).json({ messaage: "Internal server error", error: e });
+  }
+};
+
+export const deleteproduct = (req, res) => {};
--- server/routes/admin.product.js
+++ server/routes/admin.product.js
@@ -1,9 +1,16 @@
 import Router from "express";
-import { orders, products } from "../controllers/adminproductcontroller.js";
+import {
+  addproducts,
+  deleteproduct,
+  orders,
+  products,
+} from "../controllers/adminproductcontroller.js";
 import verifyjwt from "../middlewares/verifyjwt.js";
 const router = Router();
 
 router.get("/orders", verifyjwt, orders);
-router.get("/products",verifyjwt,products)
+router.get("/products", verifyjwt, products);
+router.post("/products", verifyjwt, addproducts);
+router.delete("/products/:id", verifyjwt, deleteproduct);
 
 export default router;
